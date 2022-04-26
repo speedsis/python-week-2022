afcee8bef2f72057cc136b15e7d23612f872168c
SPEED SYSTEMS SOLUCTION <speedsis@gmail.com>
2022-04-26 23:00:15 +0000

    change

---
+++ beerlog/database.py
@@ -0,0 +1,9 @@
+
+from sqlmodel import create_engine
+from beerlog.config import settings
+from beerlog import models
+
+engine = create_engine(settings.database.url)
+
+models.SQLModel.metadata.create_all(engine)
+
